e9766310a8a63f42c01503e4ce6b2d645a3596a6
Mubashir Meddekar <mubashirmeddekar@yahoo.com>
2014-11-06 18:08:16 +0530

    Sample for UIDatePicker and UIPickerView
    DatePicker and PickerView in iOS 7

--- Gestures/GestureViewController.m
+++ Gestures/GestureViewController.m
@@ -18,6 +18,17 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(onTapPressed)];
+    tap.numberOfTapsRequired = 1;
+    [self.view addGestureRecognizer:tap];
+    
+    UIPinchGestureRecognizer *myPinch = [[UIPinchGestureRecognizer alloc]initWithTarget:self action:@selector(onPinched)];
+    [self.view addGestureRecognizer:myPinch];
+    
+    UISwipeGestureRecognizer *mySwipe = [[UISwipeGestureRecognizer alloc]initWithTarget:self action:@selector(swipeDetected)];
+    
+    [self.view addGestureRecognizer:mySwipe];
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +37,16 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(IBAction)onTapPressed{
+    NSLog(@"Tap is pressed");
+}
+
+-(IBAction)onPinched{
+    NSLog(@"Pinch is detected");
+}
+
+-(IBAction)swipeDetected
+{
+    NSLog(@"Swipe is Happened");
+}
 @end
